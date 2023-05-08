f15326fe55fbaa558a84b5e12fb62726bc7c26bb
Azeem Olatunji <Azeemol@HA-SHEM.com>
2023-05-08 10:37:58 +0100

    Added Todo componet and Implement Add todo funtion

--- src/component/Todos.jsx
+++ src/component/Todos.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import {useState} from 'react'
 
 const Todos = () => {
-  return (
-    <div>Todos</div>
-  )
+    const [todos, setTodos] = useState([]);
+    const [newTodo, setNewTodo] = useState('');
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      if (!newTodo) return;
+      setTodos([...todos, newTodo]);
+      setNewTodo('');
+    };
+  
+    const handleDelete = (index) => {
+      const newTodos = [...todos];
+      newTodos.splice(index, 1);
+      setTodos(newTodos);
+    };
+  
+    return (
+      <div className="todo-list">
+        <h2>Todo List</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={newTodo}
+            onChange={(e) => setNewTodo(e.target.value)}
+            placeholder="Add a todo"
+          />
+          <button type="submit">Add</button>
+        </form>
+        <ul>
+          {todos.map((todo, index) => (
+            <li key={index}>
+              <span>{todo}</span>
+              <button onClick={() => handleDelete(index)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
 }
 
 export default Todos
