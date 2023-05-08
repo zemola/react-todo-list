e82051e65a4cf2368fb0c402d3414adcdf3a4e70
Azeem Olatunji <Azeemol@HA-SHEM.com>
2023-05-08 10:33:39 +0100

    Set up repo

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Todos from './component/Todos';
+
+function App() {
+  return (
+    <div className="App">
+        <Todos />
+    </div>
+  );
+}
+
+export default App;
